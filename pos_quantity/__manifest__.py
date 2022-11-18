df45f68935880a998814aace3b326fe89d837a2a
petr-nguyen <petr.nguyen@gusrix.com>
2022-11-18 14:20:14 +0700

    update addons

--- pos_quantity/__manifest__.py
+++ pos_quantity/__manifest__.py
@@ -10,16 +10,13 @@
     'summary': '',
     'description': "",
     'depends': ['pos'],
-    'data': [
-    ],
     'installable': True,
     'assets': {
         'pos.assets': [
-            'pos_discount/static/src/css/pos_discount.css',
-            'pos_discount/static/src/js/**/*',
+            'pos_quantity/static/src/js/**/*',
         ],
         'web.assets_qweb': [
-            'pos_discount/static/src/xml/**/*',
+            'pos_quantity/static/src/xml/**/*',
         ],
     },
     'license': 'LGPL-3',
