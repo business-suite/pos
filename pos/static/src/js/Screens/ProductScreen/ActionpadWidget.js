f5c70c44e42c8c7fa52bc69e607d129512103a3b
petr-nguyen <petr.nguyen@gusrix.com>
2022-12-15 21:16:20 +0700

    update addons

--- pos/static/src/js/Screens/ProductScreen/ActionpadWidget.js
+++ pos/static/src/js/Screens/ProductScreen/ActionpadWidget.js
@@ -1,4 +1,4 @@
-odoo.define('pos.ActionpadWidget', function(require) {
+odoo.define('pos.ActionpadWidget', function (require) {
     'use strict';
 
     const PosComponent = require('pos.PosComponent');
@@ -10,13 +10,40 @@ odoo.define('pos.ActionpadWidget', function(require) {
      * @emits click-pay
      */
     class ActionpadWidget extends PosComponent {
+
+        constructor() {
+            super(...arguments);
+            this.update();
+        }
+
+        get order() {
+            return this.env.pos.get_order();
+        }
+
+        mounted() {
+            this.order.on('change', () => {
+                this.update();
+                this.render();
+            });
+            this.order.orderlines.on('change', () => {
+                this.update();
+                this.render();
+            });
+        }
+
         get isLongName() {
             return this.client && this.client.name.length > 10;
         }
+
         get client() {
             return this.props.client;
         }
+
+        update() {
+            this.items_number = this.order ? this.order.orderlines.reduce((items_number, line) => items_number + line.quantity, 0) : 0;
+        }
     }
+
     ActionpadWidget.template = 'ActionpadWidget';
     ActionpadWidget.defaultProps = {
         isActionButtonHighlighted: false,
